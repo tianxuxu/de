76377112ee1596f0ae378cc7cb13fd9d3776c10c
tianxuxu <1552174353@qq.com>
2018-05-11 11:46:19 +0800

    Initial commit

---
+++ src/main/java/com/example/de/PageController.java
@@ -0,0 +1,31 @@
+package com.example.de;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Api(value = "PageController", description = "用户登录登出接口")
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    @ApiOperation(value="用户登录", notes="用户登录接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", required = true ,dataType = "string"),
+            @ApiImplicitParam(name = "passwd", value = "密码", required = true ,dataType = "string")
+    })
+    @RequestMapping(value = "/login",method = {RequestMethod.POST,RequestMethod.GET})
+    @ResponseBody
+    public String login(String data, HttpServletRequest request){
+        return "hello World";
+    }
+
+}
